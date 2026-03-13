a56fea993800f1dffee0e5e9a037888ac86916c5
Ali Snedden <ali.snedden@markiiisys.com>
2026-03-13 10:40:31 -0400

    code/p1ch5/2_autograd.py : 1. A commit

--- code/p1ch5/2_autograd.py
+++ code/p1ch5/2_autograd.py
@@ -7,18 +7,30 @@ t_c = torch.tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0,
 t_u = torch.tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9,
                     33.9, 21.8, 48.4, 60.4, 68.4])
 t_un = 0.1 * t_u
+
+
 def model(t_u, w, b):
     return w * t_u + b
+
+
 def loss_fn(t_p, t_c):
     squared_diffs = (t_p - t_c)**2
     return squared_diffs.mean()
+
+
 params = torch.tensor([1.0, 0.0], requires_grad=True)
-params.grad is None
+
+print(params.grad is None)
+
 loss = loss_fn(model(t_u, *params), t_c)
 loss.backward()
 params.grad
+
+
 if params.grad is not None:
     params.grad.zero_()
+
+
 def training_loop(n_epochs, learning_rate, params, t_u, t_c):
     for epoch in range(1, n_epochs + 1):
         if params.grad is not None:  # <1>\n",
